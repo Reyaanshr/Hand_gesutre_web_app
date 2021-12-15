e0ffeca8d9ba31a7bcd4c7e964421624945c64b6
Reyaanshr <76482585+Reyaanshr@users.noreply.github.com>
2021-12-15 19:47:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function takesnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML= '<img id= "captured_image" src="' + data_uri + '"/>';
+    });
+}
+
+console.log("ml5 version: ",ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/YmstY9FxR/ml5.json",modelLoaded);
+
+function modelLoaded(){
+console.log("model is loaded");
+}
+
+function speak(){
+var synth = window.speechSynthesis;
+speakdata1 = "The first prediction is " + prediction_1;
+var utterThis = new SpeechSynthesisUtterance(speakdata1);
+synth.speak(utterThis);
+}
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error, results){
+   if(error){
+       console.log(error);
+   }
+   else{
+       console.log(results);
+       document.getElementById("result_gesture_name").innerHTML = results[0].label;
+       prediction_1 = results[0].label;
+       speak();
+       if(results[0].label == "peace"){
+           document.getElementById("update_gesture").innerHTML = "&#9996;"
+       }
+
+       if(results[0].label == "thumbs up"){
+        document.getElementById("update_gesture").innerHTML = "&#128077;"
+       }
+
+       if(results[0].label == "nice"){
+           document.getElementById("update_gesture").innerHTML = "&#128076;"
+       }
+   }
+}
